91ddb9296490182de28311b6841a2e9e1d23d62c
Kristian <02kristian.shyiak@gulllakecs.org>
2020-02-19 08:47:51 -0500

    Make the button play a sound
    It works

--- app/src/main/java/com/example/board/MainActivity.java
+++ app/src/main/java/com/example/board/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.board;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button)findViewById(R.id.clickme);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.griffin);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
     }
 }
