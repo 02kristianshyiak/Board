42a26f6a7595d91b6165f71ab413d27db1f36edb
Kristian <02kristian.shyiak@gulllakecs.org>
2020-02-20 09:22:38 -0500

    Add Mutilpe buttons and make them work
    It works but you a tiny click noise

--- app/src/main/java/com/example/board/MainActivity.java
+++ app/src/main/java/com/example/board/MainActivity.java
@@ -2,25 +2,60 @@ package com.example.board;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button bt;
+    private SoundPool soundPool;
+    private int griffin, griffin2, griffin3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt = (Button)findViewById(R.id.clickme);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.griffin);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mp.start();
-            }
-        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(3)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            soundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        griffin = soundPool.load(this, R.raw.griffin, 1);
+        griffin2 = soundPool.load(this, R.raw.griffin2, 1);
+        griffin3 = soundPool.load(this, R.raw.griffin3, 1);
+    }
+    public void playSound(View v) {
+        switch (v.getId()) {
+            case R.id.button_griffin:
+                soundPool.play(griffin,1, 1, 0, 0, 1);
+                break;
+            case R.id.button_griffin2:
+                soundPool.play(griffin2,1, 1, 0, 0, 1);
+                break;
+            case R.id.button_griffin3:
+                soundPool.play(griffin3,1, 1, 0, 0, 1);
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
     }
 }
